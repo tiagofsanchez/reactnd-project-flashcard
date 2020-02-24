f16a14006a1d9c2955997ac04af2b545e7bd4d4b
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-02-24 20:18:18 +0800

    FEATURE: daily notifications implemented

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { Component } from "react";
 import { StatusBar, View } from "react-native";
 import Constants from "expo-constants";
 import { NavigationContainer } from "@react-navigation/native";
@@ -13,6 +13,7 @@ import reducer from "./reducers";
 import HomeStack from "./Components/HomeStack";
 import NewDeck from "./Components/NewDeck";
 import { pink, white } from "./utils/colors";
+import { setLocalNotification } from "./utils/helpers";
 
 function AppStatusBar({ backgroundColor, ...props }) {
   return (
@@ -23,51 +24,65 @@ function AppStatusBar({ backgroundColor, ...props }) {
 }
 
 const Tab = createBottomTabNavigator();
-function App() {
-  return (
-    <Provider store={createStore(reducer, composeWithDevTools())}>
-      <View style={{ flex: 1 }}>
-        <AppStatusBar backgroundColor={pink} barStyle="light-content" />
-        <NavigationContainer>
-          <Tab.Navigator
-            initialRouteName="Home"
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color, size }) => {
-                let icon;
-                if (route.name === "Add Deck") {
-                  icon = (
-                    <FontAwesome name="plus-square" size={size} color={color} />
-                  );
-                } else if (route.name === "Home") {
-                  icon = (
-                    <Ionicons name="ios-bookmarks" size={size} color={color} />
-                  );
+class App extends Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducer, composeWithDevTools())}>
+        <View style={{ flex: 1 }}>
+          <AppStatusBar backgroundColor={pink} barStyle="light-content" />
+          <NavigationContainer>
+            <Tab.Navigator
+              initialRouteName="Home"
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                  let icon;
+                  if (route.name === "Add Deck") {
+                    icon = (
+                      <FontAwesome
+                        name="plus-square"
+                        size={size}
+                        color={color}
+                      />
+                    );
+                  } else if (route.name === "Home") {
+                    icon = (
+                      <Ionicons
+                        name="ios-bookmarks"
+                        size={size}
+                        color={color}
+                      />
+                    );
+                  }
+                  return icon;
                 }
-                return icon;
-              }
-            })}
-            tabBarOptions={{
-              activeTintColor: Platform.OS === "ios" ? pink : white,
-              style: {
-                height: 80,
-                backgroundColor: Platform.OS === "ios" ? white : pink,
-                shadowColor: "rgba(0, 0, 0, 0.24)",
-                shadowOffset: {
-                  width: 0,
-                  height: 3
-                },
-                shadowRadius: 6,
-                shadowOpacity: 1
-              }
-            }}
-          >
-            <Tab.Screen name="Home" component={HomeStack} />
-            <Tab.Screen name="Add Deck" component={NewDeck} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </View>
-    </Provider>
-  );
+              })}
+              tabBarOptions={{
+                activeTintColor: Platform.OS === "ios" ? pink : white,
+                style: {
+                  height: 80,
+                  backgroundColor: Platform.OS === "ios" ? white : pink,
+                  shadowColor: "rgba(0, 0, 0, 0.24)",
+                  shadowOffset: {
+                    width: 0,
+                    height: 3
+                  },
+                  shadowRadius: 6,
+                  shadowOpacity: 1
+                }
+              }}
+            >
+              <Tab.Screen name="Home" component={HomeStack} />
+              <Tab.Screen name="Add Deck" component={NewDeck} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
 }
 
 export default App;
